d59294de57fa983d52c73e30ef5c0fb90db794b0
Andrei Gavrilin <gav2xlin@gmail.com>
2025-05-24 20:37:17 +0200

    ch01: update

--- Chapter01/philosophy/time/bad_time.cpp
+++ Chapter01/philosophy/time/bad_time.cpp
@@ -7,5 +7,5 @@ void example() {
   d.millis_ = 100;
 
   auto timeout = 1;     // one second
-  d.millis_ = timeout;  // an error: we meant 1000 millis but assigned just 1
+  d.millis_ = timeout;  // error: we meant 1000 milliseconds but assigned just 1
 }

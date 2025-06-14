ef4d05e1254c22a6fce7ed3ad9a9bf50602216e6
Andrei Gavrilin <gav2xlin@gmail.com>
2025-06-14 10:47:57 +0200

    ch14: deps

--- Chapter14/conanfile.py
+++ Chapter14/conanfile.py
@@ -9,10 +9,10 @@ class Pkg(ConanFile):
         self.requires("benchmark/1.9.1")
         self.requires("nanobench/4.3.11")
         self.requires("doctest/2.4.11")
-        self.requires("catch2/3.8.0")
+        self.requires("catch2/3.8.1")
         self.requires("tracy/0.11.0")
-        self.requires("libcoro/0.12.1")  # requires Linux
-        self.requires("boost/1.86.0")
+        self.requires("libcoro/0.14.1")  # requires Linux
+        self.requires("boost/1.88.0")
         if not self.settings.os == "Windows":
             self.requires("coost/3.0.2")  # const char to PCHAR on Windows
 

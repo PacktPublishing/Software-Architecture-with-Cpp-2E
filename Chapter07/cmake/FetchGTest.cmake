271b09c71021612976b37c3357dd60397d9f6a0d
Andrei Gavrilin <gav2xlin@gmail.com>
2025-07-01 18:03:46 +0200

    ch07: update

--- Chapter07/cmake/FetchGTest.cmake
+++ Chapter07/cmake/FetchGTest.cmake
@@ -1,15 +1,35 @@
-include(FetchContent)
+option(
+  USE_CPM_CMAKE
+  "Use CPM.cmake: a small CMake script for setup-free, cross-platform, reproducible dependency management"
+  ON)
 
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v1.17.0
-  EXCLUDE_FROM_ALL)
+if(NOT USE_CPM_CMAKE)
+  include(FetchContent)
 
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt
-    ON
-    CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.17.0
+    EXCLUDE_FROM_ALL)
 
-message(STATUS "GTest binaries are present at ${googletest_BINARY_DIR}")
+  # For Windows: Prevent overriding the parent project's compiler/linker
+  # settings
+  set(gtest_force_shared_crt
+      ON
+      CACHE BOOL "" FORCE)
+  FetchContent_MakeAvailable(googletest)
+
+  message(STATUS "GTest binaries are present at ${googletest_BINARY_DIR}")
+else()
+  include(CPM)
+
+  # cmake-format: off
+  CPMAddPackage(
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    GIT_TAG v1.17.0
+    VERSION 1.17.0
+    EXCLUDE_FROM_ALL
+    OPTIONS "BUILD_GMOCK OFF" "INSTALL_GTEST OFF" "gtest_force_shared_crt")
+  # cmake-format: on
+endif()

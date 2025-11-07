9e1bca90863b165e1fcb529f2cefe1aa8bbe9890
Andrei Gavrilin <gav2xlin@gmail.com>
2025-11-07 20:04:21 +0100

    ch14: update

--- Chapter14/s3/conanfile.py
+++ Chapter14/s3/conanfile.py
@@ -3,6 +3,7 @@ from conan import ConanFile
 class Pkg(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
+    default_options = {"aws-sdk-cpp/*:text-to-speech": False}
 
     def requirements(self):
         self.requires("spdlog/1.16.0")
